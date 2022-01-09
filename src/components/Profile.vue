89a93b44b8e7cb15b547dbc1ecd6134600ec2cbe
Ishin Vin <ishin.pie@gmail.com>
2022-01-09 14:42:38 +0700

    feat: add vuetify and vue router

---
+++ src/components/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="profile">
+    <img alt="Ishin Vin" src="https://avatars.githubusercontent.com/u/63244532?v=4">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile'
+}
+</script>
+
+<style lang="scss" scoped>
+.profile {
+  // width: 180px;
+  // text-align: center;
+  img{
+    // text-align: center;
+    width: 120px;
+    border: 5px solid grey;
+    border-radius: 50%;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <profile />
+  </div>
+</template>
+
+<script>
+import Profile from '../components/Profile.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Profile
+  }
+}
+</script>
